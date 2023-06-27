f843963b95df4e3346470748c8b1683eb25fb1bc
Sattu2806 <127772255+Sattu2806@users.noreply.github.com>
2023-06-27 12:52:20 +0530

    Update page.tsx(2)

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Footer from './components/Footer/Footer';
 export default function Home() {
   
   return (
-    <div className='overflow-hidden '>
+    <div className=' '>
       <div className='relative z-50'>
         <div className='absolute'>
         <Navbar/>
